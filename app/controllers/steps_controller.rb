c31b346856cfe495496500b96ca36a4eb4b16d9b
Nicholas Ng <nicholasnkw@gmail.com>
2019-06-03 16:49:34 -0500

    feature -reorder steps in a procedure when a step gets deleted

--- app/controllers/steps_controller.rb
+++ app/controllers/steps_controller.rb
@@ -26,9 +26,18 @@ class StepsController < ApplicationController
 
 	def destroy
 		@step = Step.find(params[:id])
+		if(@step.procedure_id != params[:procedure_id])
+			render json: { error: 'step does not belong to procedure' }, status: :bad_request and return
+		end
+
+		order = @step.order
 		if(@step.delete)
-			# have to reorder steps
-			head :ok
+			# e.g step 3 gets delete, step 4 til the last step all subtract one
+			p_id = params[:procedure_id]
+			count = Procedure.find(p_id).steps.count
+			# count+1 because @step is already deleted. count will drop 1, but highest order is still old count
+			@thesteps = Step.where({order: (order+1)..count+1 , procedure_id: p_id})
+			@thesteps.map { |x| x.update_attributes(order: x.order-1) }
 		else
 			head :bad_request
 		end
@@ -39,7 +48,10 @@ class StepsController < ApplicationController
 	# have to refactor to minimize query calls
 	def reorder
 		main_step = Step.find(params[:step_id])
-		#order of the previous step node of new position, 0 if new position is top
+		if(@step.procedure_id != params[:procedure_id])
+			render json: { error: 'step does not belong to procedure' }, status: :bad_request and return 
+		end
+		#new position's node before, 0 if new position is top
 		new_order_previous = params[:previous_order]
 		procedure_id = params[:id]
 		order = main_step.order
@@ -55,7 +67,7 @@ class StepsController < ApplicationController
 
 		elsif(change < 0)
 			# if change is negative, means moved down, steps between old and new position order -1
-			# e.g from order 3 to 5, means order 4, 5 all minus one
+			# e.g from order 3 to 5, means order 4, 5 all subtract one
 			@thesteps = Step.where({order: (order+1)..(new_order_previous+1), procedure_id: procedure_id})
 			@thesteps.map { |x| x.update_attributes(order: x.order-1) }
 		end
