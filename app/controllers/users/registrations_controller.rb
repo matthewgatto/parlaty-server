3a90a4243d46331da7c80406b12c54af159c289e
Pete O'Brien <raging.torrent@gmail.com>
2019-12-09 22:44:14 -0600

    Enabled invite oem - email hard coded to production for the time being.

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -89,9 +89,9 @@ class Users::RegistrationsController < Devise::RegistrationsController
   protected
 
   #If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_up_params
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :name])
-  # end
+  def configure_sign_up_params
+     devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_account_update_params
