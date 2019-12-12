d85f5c5bec7dd1e0a55a3de8c161919c8d7893ce
Pete O'Brien <raging.torrent@gmail.com>
2019-12-12 00:36:59 -0600

    Changed code to allow /users/confirmation?confirmation_token to render html

--- app/controllers/users/confirmations_controller.rb
+++ app/controllers/users/confirmations_controller.rb
@@ -13,9 +13,20 @@ class Users::ConfirmationsController < Devise::ConfirmationsController
   # end
 
   # GET /resource/confirmation?confirmation_token=abcdef
-  # def show
-  #   super
-  # end
+  def show
+    self.resource = resource_class.confirm_by_token(params[:confirmation_token])
+    yield resource if block_given?
+
+    if resource.errors.empty?
+      set_flash_message!(:notice, :confirmed)
+      #respond_with_navigational(resource){ redirect_to after_confirmation_path_for(resource_name, resource) }
+      logger.info "*** resource_name: " + resource_name.to_s
+      logger.info "*** resource: " + resource.to_s
+      render :template => "users/confirmations/show", :formats => :html
+    else
+      respond_with_navigational(resource.errors, status: :unprocessable_entity){ render :new }
+    end
+  end
 
   # POST /users/confirmation/password?confirmation_token=abcdef
   # if confirmation_token matches a user, and password 
