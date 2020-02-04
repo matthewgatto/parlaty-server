4e453f3551cfe3123319bc4003af6ea8dd29c77a
Pete O'Brien <raging.torrent@gmail.com>
2020-02-03 21:33:13 -0600

    Changed prod logging configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,14 +71,14 @@ Rails.application.configure do
   config.log_formatter = ::Logger::Formatter.new
 
   # Use a different logger for distributed setups.
-  # require 'syslog/logger'
-  # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
+  require 'syslog/logger'
+  config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'parlaty-app')
 
-  if ENV["RAILS_LOG_TO_STDOUT"].present?
-    logger           = ActiveSupport::Logger.new(STDOUT)
-    logger.formatter = config.log_formatter
-    config.logger    = ActiveSupport::TaggedLogging.new(logger)
-  end
+  #if ENV["RAILS_LOG_TO_STDOUT"].present?
+  #  logger           = ActiveSupport::Logger.new(STDOUT)
+  #  logger.formatter = config.log_formatter
+  #  config.logger    = ActiveSupport::TaggedLogging.new(logger)
+  #end
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
