d8e0b23f9ed166bb51e1e5c775dd2eea01a523b6
jdthornton <jakedthorn@gmail.com>
2020-02-06 12:32:59 -0800

    updated action reducer to handle GET devices req

--- app/javascript/src/redux/reducers/action/byId.js
+++ app/javascript/src/redux/reducers/action/byId.js
@@ -9,6 +9,7 @@ export default (state = {}, {type,payload}) => {
     case authTypes.CREATE_AUTH_REQUEST__SUCCESS:
     case deviceTypes.CREATE_DEVICE_REQUEST__SUCCESS:
     case deviceTypes.UPDATE_DEVICE_REQUEST__SUCCESS:
+    case deviceTypes.FETCH_DEVICES_REQUEST__SUCCESS:
       if(payload.actions){
         return {
           ...state,
