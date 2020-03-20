dd4c6a70e28a12caa93dcb979e8cf6a9fe634eae
jdthornton <jakedthorn@gmail.com>
2020-03-19 17:32:32 -0700

    readded time field to step form

--- app/javascript/src/components/Step/Form/index.js
+++ app/javascript/src/components/Step/Form/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useFormContext } from "react-hook-form";
 import AnimateHeight from 'react-animate-height';
 import StepHeader from '@containers/StepHeader';
 import DeviceSelect from '@containers/DeviceSelect';
@@ -6,21 +7,32 @@ import FormError from '@containers/FormError';
 import SpokenBox from '@components/Inputs/SpokenBox';
 import StepSaveButton from '@containers/StepSaveButton';
 import StepCancelButton from '@components/Step/CancelButton';
-import { Input, CheckBox, /*Select,*/ ImageInput, AudioInput, ModeRadio } from '@components/Inputs';
+import { Input, CheckBox, Select, ImageInput, AudioInput, ModeRadio } from '@components/Inputs';
 import styles from './index.module.css';
 
-export default ({isDuplicate, root, idx, title, isOpen, procedure_id, formKey, id, initialValues, /*timeOptions,*/ procedureFormKey, handleCloseForm, isDragging, provided}) => (
+
+const TIME_OPTIONS = [{value: 1, label: "1 second"}, {value: 2, label: "2 seconds"}, {value: 3, label: "3 seconds"}, {value: 4, label: "4 seconds"}, {value: 5, label: "5 seconds"}, {value: 6, label: "6 seconds"}, {value: 7, label: "7 seconds"}, {value: 8, label: "8 seconds"}]
+
+const TimeSelect = (props) => {
+  const {watch} = useFormContext();
+  const mode = watch(`${props.root}mode`);
+  return <Select {...props} disabled={mode == "continuous"} options={TIME_OPTIONS} label="Time*" name="time" />
+}
+
+export default ({isDuplicate, root, idx, title, isOpen, procedure_id, formKey, id, initialValues, procedureFormKey, handleCloseForm, isDragging, provided}) => (
   <div {...provided.dragHandleProps} {...provided.draggableProps} ref={provided.innerRef} className={styles.wrapper}>
     <StepHeader isDuplicate={isDuplicate} isOpen={isOpen ? true : false} title={title} procedure_id={procedure_id} idx={idx} id={id} root={root} handleCloseForm={handleCloseForm} />
     <AnimateHeight height={isOpen ? 'auto' : 0} duration={200} >
       <div className={styles.container}>
-        <Input as="input" defaultValue={initialValues.title} formKey={formKey} type="text" required label="Title*" root={root} name="title" />
+        <div className={styles.titleWrapper}>
         <SpokenBox idx={idx} labelClass={`${styles.spoken} align_center`} root={root} defaultValue={initialValues.spoken || false} />
-        {/*<Select defaultValue={initialValues.time || 8} formKey={formKey} options={timeOptions} label="Time*" root={root} name="time" />*/}
+        <Input as="input" defaultValue={initialValues.title} formKey={formKey} type="text" required label="Title*" root={root} name="title" />
+        </div>
         <div className={`${styles.boxes} align_center`}>
           <ModeRadio formKey={formKey} root={root} name="mode" defaultValue={initialValues.mode || "continuous"} />
           <CheckBox formKey={formKey} label="Option to Skip" root={root} name="safety" defaultValue={initialValues.safety || false} />
         </div>
+        <TimeSelect formKey={formKey} root={root} defaultValue={initialValues.time || 8} />
         <Input as="textarea" defaultValue={initialValues.location} formKey={formKey} type="text" label="Instruction" root={root} name="location" rows="4"  />
         <DeviceSelect procedure_id={procedure_id} label="Device" root={root} name="device_id" defaultDevice={initialValues.device} />
         <div>
