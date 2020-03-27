4a0677681caa603385221b3d4e9beaf42142b3bf
jdthornton <jakedthorn@gmail.com>
2020-03-26 23:28:56 -0700

    fixed import glitch

--- app/javascript/components/components/SignUpPage/index.js
+++ app/javascript/components/components/SignUpPage/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import LoginForm from '@containers/LoginForm';
+//import LoginForm from '@containers/LoginForm';
 import Polygon from '../SVG/PolygonH';
 import styles from './index.module.css';
 
@@ -15,7 +15,7 @@ const SignUpPage = ({onSubmit}) =>
       </div>
       <div className={styles.header}>Create a Parlaty account<sup className={styles.tm}>&#8482;</sup></div>
       <div className={styles.subheader}>Or <Link to="#" className={styles.underline}>Login</Link></div>
-      <LoginForm />
+      {/*<LoginForm />*/}
       <div className={styles.bottomPolygonContainer}>
         <Polygon className={styles.bottomPolygonOne} fill="#c6c6c6" stroke="#c6c6c6" size="2.7em" />
         <Polygon className={styles.bottomPolygonTwo} fill="#67318d" stroke="#67318d" size="1.4em" />
