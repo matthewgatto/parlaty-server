29c5bcf8823fafb10256418d98447e1ab2c14e7c
Pete O'Brien <raging.torrent@gmail.com>
2020-04-28 21:41:05 -0500

    Fixed 403 when client admin gets oem businesses

--- app/controllers/oem_businesses_controller.rb
+++ app/controllers/oem_businesses_controller.rb
@@ -7,6 +7,7 @@ class OemBusinessesController < ApplicationController
 		curr_user = current_user
 		is_author = curr_user.roleable_type == "Author" 
 		is_operator = curr_user.roleable_type == "Operator" 
+		is_client_admin = curr_user.roleable_type == "ClientAdmin" 
 		roleable = curr_user.roleable
 		hasOemBusiness = false
 		if is_author || is_operator
@@ -18,8 +19,8 @@ class OemBusinessesController < ApplicationController
 		end
 		is_valid = is_p_admin? || cuser_is?("Oem", @oemb.oem_id) ||
 			(is_author && hasOemBusiness) ||
-			(is_operator && hasOemBusiness)
-		puts "**** is_valid: " + is_valid.to_s
+			(is_operator && hasOemBusiness) ||
+			is_client_admin
 		if !is_valid
 			render json: {"error": "Current user access denied"}, status: :forbidden and return
 		end
