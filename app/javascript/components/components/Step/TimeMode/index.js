f15689878a4d59ce126d2ec2f639a8f0fe3bb4ee
Max Mendelev <mmendelev@navirit.by>
2020-09-21 15:52:30 +0300

    PAR-11: added safety for step

--- app/javascript/components/components/Step/TimeMode/index.js
+++ app/javascript/components/components/Step/TimeMode/index.js
@@ -1,16 +1,17 @@
-import React from "react";
+import React, {useEffect} from "react";
 import { ModeRadio, CheckBox} from '@components/Inputs';
 import {useFormContext} from "react-hook-form";
 
 
-const Mode = ({defaultValue, ...props}) => {
-  const {watch} = useFormContext();
-  let safety = watch(`${props.root}safety`);
-  if(safety) {
-    defaultValue = "manual";
-  }
+const Mode = ({root, ...props}) => {
+  const { getValues, setValue } = useFormContext();
+  let safety = getValues()[`${root}safety`];
+  useEffect(() => {
+    setValue(`${root}mode`, safety ? "manual" : "continuous");
+    safety ? setValue(`${root}time`, 0) : undefined;
+  }, [safety]);
   return (
-    <ModeRadio disabled={safety} {...props} defaultValue={defaultValue} />
+    <ModeRadio disabled={safety} root={root} {...props} />
   )
 }
 
--- app/javascript/components/redux/selectors/device.js
+++ app/javascript/components/redux/selectors/device.js
@@ -3,4 +3,4 @@ export const getDeviceMap = (state) => state.devices.byId
 export const getAllDeviceIds = (state) => state.devices.allIds
 export const getDeviceById = (id) => (state) => state.devices.byId[id];
 export const getAllDevices = (state) => state.devices.allIds.map(id => state.devices.byId[id])
-export const getDeviceActions = (id) => (state) => {id && state.devices.byId[id] && state.devices.byId[id].actions}
+export const getDeviceActions = (id) => (state) => id && state.devices.byId[id] && state.devices.byId[id].actions
