778fa8c835b7b21dfcacf83def2c63e7da823d7a
Max Mendelev <mmendelev@navirit.by>
2020-09-22 13:30:56 +0300

    PAR-11: Make tab hidden

--- app/javascript/components/components/Step/Tabs/index.js
+++ app/javascript/components/components/Step/Tabs/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, {useEffect, useState} from 'react';
 import {useSelector} from "react-redux";
 import PropTypes from 'prop-types';
 import AppBar from '@material-ui/core/AppBar';
@@ -8,7 +8,7 @@ import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
 import { getStepValues } from '@selectors/template'
 import { makeStyles, withStyles } from '@material-ui/core/styles';
-
+import styles from './index.module.css';
 import {ArrFileInput} from '@components/Inputs';
 import DeviceSelect from '@containers/DeviceSelect';
 import LoopForm from '@components/Step/LoopForm';
@@ -72,6 +72,13 @@ const CustTab = withStyles({
     backgroundColor: '#ddd',
   },
 })(Tab);
+const HiddenTab = withStyles({
+  root: {
+    maxWidth: '0',
+    minWidth: '0',
+    padding: '0',
+  },
+})(Tab);
 function tabProps(index) {
   return {
     id: `scrollable-auto-tab-${index}`,
@@ -88,24 +95,29 @@ export default ({ initialValues, formKey, root, procedure_id, idx, updateParams 
     setValues({...values, ...updateValue});
     setTab(newValue);
   };
+  useEffect(() => {
+    setValues({...values, ...updateValue});
+  }, [initialValues])
   const looped = values.looped_by;
-  const disabledTab = looped !== -1 && looped !== idx+1;
+  const disabledTab = looped !== undefined && looped !== -1 && looped !== idx+1;
   return (
     <div>
       <AppBar position="static" color="default" className={classes.header}>
         <CustTabs value={tab} onChange={handleChange} indicatorColor="primary" textColor="primary" aria-label="auto tabs example" centered >
-          <CustTab label="Device" {...tabProps(0)} />
-          <CustTab label="Loop" disabled={disabledTab} {...tabProps(1)} />
-          <CustTab label="Media" {...tabProps(2)} />
+          <HiddenTab label="" {...tabProps(0)} />
+          <CustTab label="Device" {...tabProps(1)} />
+          <CustTab label="Loop" disabled={disabledTab} {...tabProps(2)} />
+          <CustTab label="Media" {...tabProps(3)} />
         </CustTabs>
       </AppBar>
-      <TabPanel tab={tab} index={0}>
+      <TabPanel tab={tab} className={styles.withoutPadding} index={0}/>
+      <TabPanel tab={tab} index={1}>
         <DeviceSelect onChange={updateParams} procedure_id={procedure_id} label="Device" root={root} idx={idx} name="device_id" device_id={values.device_id} defaultValue={values.device_id} />
       </TabPanel>
-      <TabPanel tab={tab} index={1}>
+      <TabPanel tab={tab} index={2}>
         <LoopForm onChange={updateParams} formKey={formKey} defaultValue={values} root={root}/>
       </TabPanel>
-      <TabPanel tab={tab} index={2}>
+      <TabPanel tab={tab} index={3}>
         <div>
           <ArrFileInput setTabValues={setValues} name="media" label="Media*" formKey={formKey} idx={idx} defaultValues={values.visuals || undefined} root={root} objName={'step'}
                         radio={{isShown: true, params: [{type: 'image', label: 'Default media'},{type: 'video', label: 'Default media'}], actionRoot: 'default_media', defaultValue: values.default_media, withoutChecked: true}}
