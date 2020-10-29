693b8f35eac0a5fb51fe43170a33f677adca1dea
Max Mendelev <mmendelev@navirit.by>
2020-10-29 19:56:38 +0300

    PAR-38: changed Time mode selection logic for steps

--- app/javascript/components/components/Step/TimeMode/index.js
+++ app/javascript/components/components/Step/TimeMode/index.js
@@ -3,22 +3,22 @@ import { ModeRadio, CheckBox} from '@components/Inputs';
 import {useFormContext} from "react-hook-form";
 
 
-const Mode = ({root, ...props}) => {
+const Mode = ({root, defaultValue, ...props}) => {
   const { getValues, setValue } = useFormContext();
   let safety = getValues()[`${root}safety`];
   useEffect(() => {
-    setValue(`${root}mode`, safety ? "manual" : "continuous");
+    setValue(`${root}mode`, safety ? "manual" : defaultValue.mode || "continuous");
     safety ? setValue(`${root}time`, 0) : undefined;
   }, [safety]);
   return (
-    <ModeRadio disabled={safety} root={root} {...props} />
+    <ModeRadio disabled={safety} root={root} defaultValue={defaultValue.mode || "continuous"} {...props} />
   )
 }
 
 export default ({formKey, defaultValue, root , onChange}) => {
   return(
     <>
-      <Mode onChange={onChange} formKey={formKey} root={root} name="mode" defaultValue={defaultValue.mode || "continuous"}/>
+      <Mode onChange={onChange} formKey={formKey} root={root} name="mode" defaultValue={defaultValue}/>
       <CheckBox onChange={onChange} formKey={formKey} label="Safety" root={root} name="safety" defaultValue={defaultValue.safety || false} />
     </>
   )
