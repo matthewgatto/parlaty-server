e2b6022f0011dd7e915ae0181a08a67449816470
Max Mendelev <dmitry.k@bitsol.org>
2020-12-02 20:52:03 +0300

    PAR-14: add doc file views and uploading media files

--- app/javascript/components/containers/activeModal.js
+++ app/javascript/components/containers/activeModal.js
@@ -5,7 +5,11 @@ import { getModal } from '@selectors/modal';
 export default (WrappedComponent, modalType) => (props) => {
   const modal = useSelector(getModal);
   if (modal && modal.type === modalType) {
-    return <WrappedComponent {...props} modalData={modal.data && modal.data.isArrParams ? modal.data.src : modal.data}/>
+    return <WrappedComponent
+      {...props}
+      modalType={modal.data && modal.data.fileType || null}
+      modalData={modal.data && modal.data.isArrParams ? modal.data.src : modal.data}
+    />
   }
   return null;
 }
