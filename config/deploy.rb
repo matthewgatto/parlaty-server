8600b10fecaf555876f5df0c2681ea381edfb5da
Tom W <rampsorlen@gmail.com>
2022-03-29 20:52:24 +0000

    update deploy version

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.11.2"
+lock "~> 3.17.0"
 
 set :application, "parlaty-server"
 #set :repo_url, "https://aaa:bbbb@bitbucket.org/jedmahonisgroup/parlaty-server.git"
