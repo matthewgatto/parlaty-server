129b2fa4fdf6cee9f865d30c718c6bb43dd21144
Pete O'Brien <pjo@usa.com>
2022-04-01 17:50:09 -0500

    Add Card Modal dialog mostly.

--- app/javascript/components/components/Oem/Content/index.js
+++ app/javascript/components/components/Oem/Content/index.js
@@ -1,21 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 import useOemInfo from '@containers/useOemInfo';
 import Label from '@components/List/Label';
 import SubmitButton from '@components/SubmitButton';
 import ModalTrigger from '@containers/ModalTrigger';
 import ModalOverlay from '@components/Modal/Overlay';
 import styles from './index.module.css';
+import ShowStripeElementModal from "../ShowStripeElementModal";
 
-function getClientSecret(oem_id) {
-  let paymentIntent = oem && oem.subscription && oem.subscription.payment_intent && oem.subscription.payment_intent.client_secret;
-  if (paymentIntent) {
-    return paymentIntent;
-  } else {
-    return null;
-  }
-}
+export default ({oem_id}) => {
+  const [posts, setPosts] = useState([]);
+  const fetchPost = async () => {
+    const localData = localStorage.getItem('login_data_4_16');
+    if (localData) {
+      let token = JSON.parse(localData).jwt
+      const url = `/oems/${oem_id}/setup_intent`
+      try {
+        const response = await fetch(url, {
+              method: 'GET',
+              headers: {
+                'Authorization': `Bearer ${token}`
+              }
+            }
+        )
+        const data = await response.json();
+        setPosts(data);
+      } catch (e) {
+        console.error(`exception ${e} during fetch of ${url}`)
+      }
+    }
+  };
+  useEffect(() => {
+    fetchPost();
+  }, []);
 
-export default ({ oem_id }) => {
+  const clientSecret = posts && posts.setup_intent && posts.setup_intent.client_secret ?
+      posts.setup_intent.client_secret : null
   let oem = useOemInfo(oem_id);
   let oemStatus = oem && oem.subscription && oem.subscription.subscription_status && oem.subscription.subscription_status != "PENDING" ? oem.subscription.subscription_status : "INACTIVE";
   let userCount = oem && oem.subscription && oem.subscription.user_count ? oem.subscription.user_count : 0;
@@ -23,7 +42,7 @@ export default ({ oem_id }) => {
   let subscriptionPlans = oem && oem.subscription && oem.subscription.plans ? oem.subscription_plans : [];
   let paymentLabel = oem && oem.source_id ? "Update Card" : "Add Card";
   let planLabel = subscriptionPlan ? "Add Subscription" : "Update Subscription";
-  let modalKey = {clientSecret: "TESTING"};
+  let modalKey = {clientSecret: clientSecret};
   let modalPlans = {subscriptionPlans: subscriptionPlans};
   return (<>
     <Label>Subscription</Label>
@@ -41,10 +60,15 @@ export default ({ oem_id }) => {
         <span className={styles.text}>{userCount}</span>
       </div>
       <div className={styles.flex}>
-        <ModalTrigger modal="show_stripe_element" modalData={modalKey}><SubmitButton primary className={styles.margins} label={paymentLabel}/></ModalTrigger>
-        <ModalTrigger modal="show_subscription_plans" modalData={modalPlans}><SubmitButton primary className={styles.margins} label={planLabel}/></ModalTrigger>
+        <ModalTrigger modal="show_stripe_element">
+          <SubmitButton primary className={styles.margins}
+                        label={paymentLabel}/>
+        </ModalTrigger>
       </div>
     </div>
+    <ModalOverlay>
+      <ShowStripeElementModal modalKey={modalKey}/>
+    </ModalOverlay>
   </>)
 }
 
--- app/javascript/components/components/Oem/ShowStripeElementModal/index.js
+++ app/javascript/components/components/Oem/ShowStripeElementModal/index.js
@@ -1,22 +1,79 @@
-import React from 'react';
-import {Elements} from '@stripe/react-stripe-js';
+import React, {useState} from 'react';
+import {PaymentElement, CardElement, Elements, useElements, useStripe} from '@stripe/react-stripe-js';
 import {loadStripe} from '@stripe/stripe-js';
 import activeModal from '@containers/activeModal';
+import ModalTrigger from '@containers/ModalTrigger';
+import SubmitButton from '@components/SubmitButton';
+import styles from "../../Modal/DeleteConfirmation/index.module.css";
 
 const stripePromise = loadStripe(process.env.STRIPE_PUBLIC_KEY);
 
+const SetupForm = () => {
+    const stripe = useStripe();
+    const elements = useElements();
 
-export default activeModal(({client_secret}) => {
-  const options = {
-    clientSecret: client_secret
-  };
+    const [errorMessage, setErrorMessage] = useState(null);
+    const [confirmMessage, setConfirmMessage] = useState(null);
+    const [disabled, setDisabled] = useState(false);
 
-  return (
-    <>
-      <Elements stripe={stripePromise} options={options}>
-        <CardElement />
-        <button>Submit</button>
-      </Elements>
-    </>
-  )
-});
+    const handleSubmit = async (event) => {
+        // We don't want to let default form submission happen here,
+        // which would refresh the page.
+        event.preventDefault();
+        setErrorMessage("")
+        setConfirmMessage("")
+        if (!stripe || !elements) {
+            // Stripe.js has not yet loaded.
+            // Make sure to disable form submission until Stripe.js has loaded.
+            console.log('Stripe.js has not yet loaded.')
+            return;
+        }
+
+        const {error} = await stripe.confirmSetup({
+            //`Elements` instance that was used to create the Payment Element
+            elements,
+            redirect: "if_required"
+        });
+
+        if (error) {
+            // This point will only be reached if there is an immediate error when
+            // confirming the payment. Show error to your customer (for example, payment
+            // details incomplete)
+            console.error(error.message + '- error occurred during stripe confirm setup')
+            setErrorMessage(error.message);
+        } else {
+            // Your customer will be redirected to your `return_url`. For some payment
+            // methods like iDEAL, your customer will be redirected to an intermediate
+            // site first to authorize the payment, then redirected to the `return_url`.
+            console.log('Stripe confirm succeeded')
+            setConfirmMessage('Add Card completed successfully.');
+            setDisabled(true);
+        }
+        return
+    }; // handleSubmit
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <PaymentElement />
+            <div className={styles.buttons}>
+              <button disabled={disabled} className="primary button align_center">Add Card</button>
+                <ModalTrigger>
+                    <button className="primary button align_center">Close</button>
+                </ModalTrigger>
+            </div>
+            {errorMessage && <div style={{marginTop: '0.25rem', color: '#df1b41', fontWeight: '400', fontSize: '1.93rem'}}>{errorMessage}</div>}
+            {confirmMessage && <div style={{marginTop: '0.25rem', color: '#30b130', fontWeight: '400', fontSize: '1.93rem'}}>{confirmMessage}</div>}
+        </form>
+    );
+};
+
+export default activeModal(({modalKey}) => {
+    const options = {
+        clientSecret: modalKey.clientSecret
+    }
+    return (
+        <Elements stripe={stripePromise} options={options}>
+      <SetupForm/>
+    </Elements>
+        );
+}, "show_stripe_element")
